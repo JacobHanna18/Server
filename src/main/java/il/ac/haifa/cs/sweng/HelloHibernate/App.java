bde3504f09cb6af89dba866b95c0a24eff3c6863
JacobHanna18 <jacob18111998@gmail.com>
2020-06-15 15:48:46 +0300

    Hibernate Connection

--- src/main/java/il/ac/haifa/cs/sweng/HelloHibernate/App.java
+++ src/main/java/il/ac/haifa/cs/sweng/HelloHibernate/App.java
@@ -1,28 +1,18 @@
 package il.ac.haifa.cs.sweng.HelloHibernate;
 
-import java.awt.desktop.PrintFilesEvent;
-import java.io.Serializable;
-import java.util.Arrays;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
 
-import org.hibernate.FlushMode;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
-
-import il.ac.haifa.cs.sweng.HelloHibernate.entities.Course;
-import il.ac.haifa.cs.sweng.HelloHibernate.entities.Lecturer;
-import il.ac.haifa.cs.sweng.HelloHibernate.entities.Student;
 
 public class App 
 {
